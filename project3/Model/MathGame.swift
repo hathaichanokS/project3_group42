3be9cb3cfc19ddbfec2c80ce9cb09ec368d3e523
Hathaichanok Sreecharoen <imac@192-168-1-10.tpgi.com.au>
2018-05-20 12:10:43 +1000

    create model class

---
+++ project3/Model/MathGame.swift
@@ -0,0 +1,9 @@
+//
+//  MathGame.swift
+//  project3
+//
+//  Created by Hathaichanok Sreecharoen on 20/5/18.
+//  Copyright © 2018 Hathaichanok Sreecharoen. All rights reserved.
+//
+
+import Foundation
